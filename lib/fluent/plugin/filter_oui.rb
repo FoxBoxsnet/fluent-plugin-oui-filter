5e80bf1e59bd09ac5e1db99c9cc7f91ecad985a2
foxboxsnet <naokiaoyama2009@gmail.com>
2016-11-23 21:46:24 +0900

    up

--- lib/fluent/plugin/filter_oui.rb
+++ lib/fluent/plugin/filter_oui.rb
@@ -20,9 +20,9 @@ module Fluent::Plugin
       tag = tag.sub(@remove_prefix, '') if @remove_prefix
       tag = (@add_prefix + '.' + tag) if @add_prefix
 
-      es.each do |time.now, record|
+      es.each do |record|
         record[@key_prefix] = getprotocolname(record[@mac_address])
-        new_es.add(time.now, record)
+        new_es.add(record)
       end
       return new_es
     end
