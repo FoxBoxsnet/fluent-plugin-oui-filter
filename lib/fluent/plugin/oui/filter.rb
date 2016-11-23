e17e1a26a8eaffd20a394bbb1bf83e95330ef2c2
Naoki Aoyama <n.aoyama@homesoc.tokyo>
2016-11-23 14:53:19 +0900

    init commit

---
+++ lib/fluent/plugin/oui/filter.rb
@@ -0,0 +1,11 @@
+require "fluent/plugin/oui/filter/version"
+
+module Fluent
+  module Plugin
+    module Oui
+      module Filter
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/fluent/plugin/oui/filter/version.rb
@@ -0,0 +1,9 @@
+module Fluent
+  module Plugin
+    module Oui
+      module Filter
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
