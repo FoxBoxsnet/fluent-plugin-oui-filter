1c84f9093a2c9c16b0f47caac162260cfa82c2ca
foxboxsnet <naokiaoyama2009@gmail.com>
2016-11-24 18:47:37 +0900

    up

--- lib/fluent/plugin/filter_oui.rb
+++ lib/fluent/plugin/filter_oui.rb
@@ -22,8 +22,7 @@ module Fluent::Plugin
       tag = (@add_prefix + '.' + tag) if @add_prefix
 
         es.each do |time, record|
-          record[@key_prefix] = getouiname(record[@mac_address])
-        rescue nil
+          record[@key_prefix] = getouiname(record[@mac_address]) rescue nil
           new_es.add(time, record)
         end
         return new_es
